71008d4880eebfb1bb1eb167a0880df05ec722e5
xmavv <xxmatheooo@gmail.com>
2024-09-23 22:07:17 +0200

    uploading a cabin with photo to supabase, including bucket upload and nice input style in ui

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -12,9 +12,14 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  //dajemy replace bo supabase utworzy folder gdy sa jakies slashe w stringu
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replace("/", "");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. create a cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
@@ -22,6 +27,22 @@ export async function createCabin(newCabin) {
     throw new Error("cabin could not be created!");
   }
 
+  // 2. upload image only if there is no error
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin if tehre was an error uploading image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created!"
+    );
+  }
+
+  // musimy tak czy siak zwrocic bo chyba react query sobie robi awaita na ta funkcje
+  // i w ten sposob daje isLoading state
   return data;
 }
 
