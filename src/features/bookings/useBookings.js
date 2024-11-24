b059772f9cbb0a04164cdae36c9bc64d5d3b2ba2
xmavv <xxmatheooo@gmail.com>
2024-11-24 03:46:19 +0100

    add filtering bookings on db(api) side

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
   const {
     data: bookings,
     isLoading,
     status,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { bookings, isLoading, status };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,19 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+
+  //FILTER
+  if (filter != null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+  //TU MOZNA JAKOS ZROBIV TABLICE TYCH OBIEKTOW I JAKOS TO MAPOWAC POMYSL NAD TYM
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
